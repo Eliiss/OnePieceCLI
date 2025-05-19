3b2e570dfd586f88e48cc131c713eb8f3e1e74e1
Eliiss <elizabethoppongansah@gmail.com>
2025-05-19 11:03:58 +0100

    Add project files.

---
+++ DataTypes.fs
@@ -0,0 +1,43 @@
+﻿module DataTypes
+
+
+type Direction = North | East | West 
+
+type Item =
+    { Id: string          
+      Name: string         
+      Description: string   
+      IsKeyPathItem: bool 
+      RiddleId: string option // (Set of Riddle Identifiers ∪ {None})
+    }
+
+
+type RiddleState = NotSolved | Solved
+
+type Riddle =
+    { RiddleText: string
+      Solution: string         
+      RewardItemId: string     
+      CurrentState: RiddleState
+    }
+
+type NextLocationRequirement =
+    { RequiredItemId: string option
+    }
+
+type Location =
+    { Id: string
+      Name: string
+      Description: string
+      UnavailableItems: Map<string, Item> 
+      AvailableItems: Map<string, Item>   
+      NextLocation: Map<Direction, string>  
+      Riddles: Map<string, Riddle>       
+      NextLocationUnlockRequirements: Map<Direction, NextLocationRequirement>
+    }
+
+type Player =
+    { CurrentLocationId: string
+      Inventory: list<Item>
+    }
+
---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿open DataTypes
+
+
+[<EntryPoint>]
+let main argv =
+    let startDirection = North // Uses Direction DU from DomainTypes
+    let testItem : Item = { Id="test001"; Name="Test Item"; Description="A simple test."; IsKeyPathItem=false; RiddleId=None }
+
+    printfn "Direction: %A" startDirection
+    printfn "Item Name: %s" testItem.Name
+    0 
